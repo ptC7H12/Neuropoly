861edfab826b85b5a2c4456fa1197c0d365599c2
ptC7H12 <ptC7H12@users.noreply.github.com>
2026-02-16 12:30:25 +0100

    Create incremental model for LightGBM training to handle large datasets in batches

--- pipeline/incremental_model.py
+++ pipeline/incremental_model.py
@@ -3,29 +3,32 @@ import numpy as np
 import pandas as pd
 
 class IncrementalLightGBM:
-    def __init__(self, **kwargs):
-        self.model = lgb.LGBMClassifier(**kwargs)
-        self.X = pd.DataFrame()
-        self.y = pd.Series()
+    def __init__(self, params=None):
+        self.params = params if params else {}
+        self.model = None
 
-    def add_data(self, X_new, y_new):
-        self.X = pd.concat([self.X, X_new], ignore_index=True)
-        self.y = pd.concat([self.y, y_new], ignore_index=True)
+    def fit(self, X, y, batch_size=1024):
+        # Initialize model if none exists
+        if self.model is None:
+            self.model = lgb.LGBMClassifier(**self.params)
 
-    def incremental_train(self):
-        self.model.fit(self.X, self.y, init_score=np.zeros(len(self.y)), verbose=False)
+        # Process data in batches
+        for start in range(0, len(X), batch_size):
+            end = min(start + batch_size, len(X))
+            X_batch = X[start:end]
+            y_batch = y[start:end]
 
-    def predict(self, X_test):
-        return self.model.predict(X_test)
+            # Fit model incrementally
+            self.model.fit(X_batch, y_batch, init_score=self.model.predict_proba(X_batch)[:, 1], verbose=False,
+                           eval_set=[(X_batch, y_batch)], eval_names=['train'], eval_metric='logloss', early_stopping_rounds=10)
+
+    def predict(self, X):
+        if self.model is None:
+            raise Exception('Model has not been trained yet!')
+        return self.model.predict(X) 
 
     def save_model(self, filename):
-        self.model.save_model(filename)
+        self.model.booster_.save_model(filename)
 
     def load_model(self, filename):
         self.model = lgb.Booster(model_file=filename)
-
-# Example usage:
-# model = IncrementalLightGBM()
-# model.add_data(X_new_train, y_new_train)
-# model.incremental_train()
-# predictions = model.predict(X_test)
